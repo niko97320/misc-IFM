b24773281d1b5e6bf6d99340001d6e1ac33fde5e
Nicolas Martin <nicolasmartin973@gmail.com>
2016-09-20 09:16:23 +0200

    Add files via upload

---
+++ density.py
@@ -0,0 +1,20 @@
+# this script plots teh density distribution of a time series
+# using the KDE method
+# Nicolas Matin 19/09/16
+
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats.kde import gaussian_kde
+import sys
+
+inp=sys.argv[1]
+col=int(sys.argv[2])
+
+print "Reading data from %s" %inp
+print "Reading column %i" %col
+
+data = np.genfromtxt(inp, usecols=col)
+kde = gaussian_kde(data)
+dist_space = np.linspace( min(data), max(data), 100)
+plt.plot(dist_space, kde(dist_space), color=np.random.rand(3,1) , lw=3)
+plt.show()
